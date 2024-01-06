e9ede73dc572451a7c2bdf588bf90e87d722fc71
Qmpzlawasd <diaconustefan16@gmail.com>
2024-01-06 18:40:25 +0200

    Add fonts

--- Flight Game/Assets/Scripts/GUI/Crosshair.cs
+++ Flight Game/Assets/Scripts/GUI/Crosshair.cs
@@ -14,12 +14,15 @@ public class Crosshair : MonoBehaviour
     [SerializeField]
     private RectTransform _velocityCrosshair;
 
+    [SerializeField]
+    private float _offset;
+
     void Update()
     {
         _directionCrosshair.localPosition = _camera.WorldToScreenPoint(_camera.transform.position + _plane.transform.forward)
-                                            - new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2);
+                                            - new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2) - Vector3.up * _offset;
 
         _velocityCrosshair.localPosition = _camera.WorldToScreenPoint(_camera.transform.position + _plane.velocity.normalized)
-                                            - new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2);
+                                            - new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2) - Vector3.up * _offset;
     }
 }
